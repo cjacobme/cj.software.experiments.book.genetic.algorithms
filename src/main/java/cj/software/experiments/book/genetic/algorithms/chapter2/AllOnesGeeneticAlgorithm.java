1b5f1e54542d2d9e033b6ba74fdb3d59bc135ca9
cjacobme <cjacobme@aol.com>
2022-08-25 20:50:00 +0200

    bootstrap

---
+++ src/main/java/cj/software/experiments/book/genetic/algorithms/chapter2/AllOnesGeeneticAlgorithm.java
@@ -0,0 +1,11 @@
+package cj.software.experiments.book.genetic.algorithms.chapter2;
+
+import cj.software.experiments.book.genetic.algorithms.chapter2.entity.GeneticAlgorithm;
+
+public class AllOnesGeeneticAlgorithm
+{
+	public static void main(String[] args)
+	{
+		GeneticAlgorithm geneticAlgorithm = new GeneticAlgorithm(100, 0.01, 0.95, 0);
+	}
+}
