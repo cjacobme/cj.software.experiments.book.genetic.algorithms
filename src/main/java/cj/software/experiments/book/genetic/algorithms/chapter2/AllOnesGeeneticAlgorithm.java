78869fd39fe6c4f55d5b50eebb114f58f32d04b5
cjacobme <cjacobme@aol.com>
2022-08-27 21:16:28 +0200

    use a logger instead of System.out

--- src/main/java/cj/software/experiments/book/genetic/algorithms/chapter2/AllOnesGeeneticAlgorithm.java
+++ src/main/java/cj/software/experiments/book/genetic/algorithms/chapter2/AllOnesGeeneticAlgorithm.java
@@ -1,11 +1,16 @@
 package cj.software.experiments.book.genetic.algorithms.chapter2;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import cj.software.experiments.book.genetic.algorithms.chapter2.entity.Individual;
 import cj.software.experiments.book.genetic.algorithms.chapter2.entity.Population;
 import cj.software.experiments.book.genetic.algorithms.chapter2.impl.PopulationService;
 
 public class AllOnesGeeneticAlgorithm
 {
+	private static Logger logger = LogManager.getFormatterLogger();
+
 	public static void main(String[] args)
 	{
 		PopulationService populationService = new PopulationService();
@@ -29,8 +34,7 @@ public class AllOnesGeeneticAlgorithm
 		while (!populationService.isTerminationConditionMet(population))
 		{
 			Individual fittest = populationService.getFittest(population, 0);
-			// TODO use logger
-			System.out.println(String.format("Best solution[%2d]: %s", generation, fittest));
+			logger.info("Best solution[%2d]: %s", generation, fittest);
 
 			// apply crossover
 			population = populationService.crossOver(population, crossOverRate, elitismCount);
@@ -44,8 +48,8 @@ public class AllOnesGeeneticAlgorithm
 			generation++;
 		}
 
-		System.out.println("Found solution after " + generation + " cycles");
+		logger.info("Found solution after %d cycles", generation);
 		Individual fittest = populationService.getFittest(population, 0);
-		System.out.println("fittest is " + fittest);
+		logger.info("fittest is %s", fittest);
 	}
 }
